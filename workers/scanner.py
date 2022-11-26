076b5e5dc0eb46d28e9ec8896ac8c972a8da8b05
m <m.soshnev@emzior.ru>
2022-11-26 20:47:19 +0300

    add proc scanner

---
+++ workers/scanner.py
@@ -0,0 +1,19 @@
+import psutil
+from utils import Logger
+
+
+class Scanner:
+    def __init__(self):
+        self._procs = []
+        self._logger = Logger(self.__class__.__name__)
+
+        self._logger.debug("Init %s", self.__class__.__name__)
+
+    def scan(self):
+        self._logger.debug("Scanning...")
+
+        self._procs.clear()
+        for proc in psutil.process_iter():
+            self._procs.append(proc)
+
+        self._logger.debug("Scanned %d process", len(self._procs))
